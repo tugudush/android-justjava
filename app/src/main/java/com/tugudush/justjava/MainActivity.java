09a5646478041a23b03f6b6ea65bf253a44ab2cf
tugudush <jerome2kph@gmail.com>
2017-09-07 00:41:21 +0800

    updates

--- app/src/main/java/com/tugudush/justjava/MainActivity.java
+++ app/src/main/java/com/tugudush/justjava/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
+
 import java.text.NumberFormat;
 
 
@@ -19,18 +20,33 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submit_order(View view) {
-        int coffees = 2;
+        /*int coffees = 2;
         display(coffees);
-        displayPrice(coffees * 5);
+        displayPrice(coffees * 5);*/
     }
 
     public void increment(View view) {
-
-    }
+        TextView qty = (TextView) findViewById(R.id.qty);
+        String qty_string = qty.getText().toString();
+        int coffees = Integer.parseInt(qty_string);
+        coffees = coffees + 1;
+        display(coffees);
+        displayPrice(coffees * 5);
+    } // end of public void increment(View view)
 
     public void decrement(View view) {
+        TextView qty = (TextView) findViewById(R.id.qty);
+        String qty_string = qty.getText().toString();
+        int coffees = Integer.parseInt(qty_string);
 
-    }
+        if (coffees <= 1) {
+
+        } else {
+            coffees = coffees - 1;
+            display(coffees);
+            displayPrice(coffees * 5);
+        }
+    } // end of public void decrement(View view)
 
     /**
      * This method displays the given quantity value on the screen.
